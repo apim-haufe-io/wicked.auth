70188ce16afc9b56354feef2b1a3e8edb0c7e80c
Martin Danielsson <martin@danielsson.de>
2018-06-25 11:29:03 +0200

    Refactor out build to separate script

---
+++ build-typescript.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$(which tsc)" ]; then
+    echo "ERROR: TypeScript must be installed; run"
+    echo "  npm install -g typescript"
+    echo "Then try again."
+    exit 1
+fi
+
+tsc 
+
+rm -rf ./dist/views
+cp -rf ./src/views ./dist
+cp -f package.json ./dist
+rm -rf ./dist/assets
+mkdir -p ./dist/assets/jquery
+mkdir -p ./dist/assets/bootstrap
+cp -rf ./node_modules/bootstrap/dist ./dist/assets/bootstrap
+cp -rf ./node_modules/jquery/dist ./dist/assets/jquery
